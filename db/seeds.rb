fb0f51d8d6b65da5116e009de6ed06e8ecb6e4d9
Chris Henry <cdhenry@gmail.com>
2018-04-11 00:34:13 +0000

    created some faker data to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+  Destinations.create(
+    name: Faker::HitchhikersGuideToTheGalaxy.location
+    description: Faker::Hipster.paragraph
+    city: Faker::Address.city
+    state: Faker::Address.state
+    street_address: Faker::Address.street_address
+  )
+  RoadTrips.create(
+    author_id: rand[1..10]
+    title: Faker::Book.title
+    description: Faker::Hipster.paragraph
+  )
+  Tags.create(
+    title: Faker::Hipster.word
+  )
+  car_make = Faker::Vehicle.make
+  Cars.create(
+    make: car_make
+    model: Faker::Vehicle.models_by_make.car_make
+    mpg: rand[20..60]
+  )
+  Users.create(
+    username: Faker::Internet.user_name
+    email: Faker::Internet.email
+    password: Faker::Internet.password(6)
+  )
+end
+
+
+#
+# counter = 1
+# Item.all.each do |item|
+#   item.category_id = counter
+#   item.save
+#   counter += 1
+# end
+# FactoryBot.create(:user)
