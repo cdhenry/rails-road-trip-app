50565b5a727d2f7210378f0c5e915f173f6f2a81
Chris Henry <cdhenry@gmail.com>
2018-04-11 01:28:58 +0000

    fixed syntax errors and created car make/model arrays for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open("https://www.motability.co.uk/cars-scooters-and-powerchairs/types-of-cars/by-makes-and-models"))
+
+make = doc.search("tbody td")
+makes = make.children.collect.with_index(1){|make,i| make.text if i.odd?}.compact
+models = make.children.collect.with_index(1){|model,i| model.text if i.even?}.compact
+
 10.times do
-  Destinations.create(
-    name: Faker::HitchhikersGuideToTheGalaxy.location
-    description: Faker::Hipster.paragraph
-    city: Faker::Address.city
-    state: Faker::Address.state
+  Destination.create(
+    name: Faker::HitchhikersGuideToTheGalaxy.location,
+    description: Faker::Hipster.paragraph,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
     street_address: Faker::Address.street_address
   )
-  RoadTrips.create(
-    author_id: rand[1..10]
-    title: Faker::Book.title
+  RoadTrip.create(
+    author_id: rand(1..10),
+    title: Faker::Book.title,
     description: Faker::Hipster.paragraph
   )
-  Tags.create(
+  Tag.create(
     title: Faker::Hipster.word
   )
-  car_make = Faker::Vehicle.make
-  Cars.create(
-    make: car_make
-    model: Faker::Vehicle.models_by_make.car_make
-    mpg: rand[20..60]
+  Car.create(
+    make: makes.sample,
+    model: models.sample,
+    mpg: rand(20..60)
   )
-  Users.create(
-    username: Faker::Internet.user_name
-    email: Faker::Internet.email
+  User.create(
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.email,
     password: Faker::Internet.password(6)
   )
 end
