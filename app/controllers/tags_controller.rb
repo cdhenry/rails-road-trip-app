1eac6140b10d66516480dc8dad1764ffd3da0a69
Chris Henry <cdhenry@gmail.com>
2018-04-13 23:12:37 +0000

    ordered tags index page by popularity

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
   before_action :set_tag, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tags = Tag.all
+    @tags = Tag.popularity_ordered
   end
 
   def show
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,6 +4,10 @@ class Tag < ActiveRecord::Base
   has_many :destination_tags
   has_many :destinations, through: :destination_tags
 
+  def self.popularity_ordered
+    joins(:destination_tags).group("destination_tags.tag_id").order("COUNT(destination_tags.tag_id) DESC")
+  end
+
   def popularity
     self.destinations.count
   end
