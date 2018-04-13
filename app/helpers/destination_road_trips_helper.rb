b0bddce4dbb1b4373001178d7c90eda7f5c7f8ce
Chris Henry <cdhenry@gmail.com>
2018-04-13 20:02:39 +0000

    road trip show page now show destinations in order

--- app/helpers/destination_road_trips_helper.rb
+++ app/helpers/destination_road_trips_helper.rb
@@ -1,4 +1,8 @@
 module DestinationRoadTripsHelper
+  def drt_destination_finder(drt)
+    Destination.find(drt.destination_id)
+  end
+
   def drts_find_or_build(road_trip, destination)
     drt = DestinationRoadTrip.where(road_trip_id: road_trip.id).where(destination_id: destination.id).first
     if !drt.nil?
